52bc46e0188133a28b1262e5254ff1ad06ac0699
Saranyan Senthivel <ssenthivel2@dxc.com>
2019-12-06 12:23:06 -0600

    New Updates

--- LeetCode/largestsubstring.py
+++ LeetCode/largestsubstring.py
@@ -1,13 +1,17 @@
 class Solution:
     def lengthOfLongestSubstring(self, s):
         start = maxLength = 0
+        # Used Characters hashmap or set to hold the values
         usedChar = {}
         for index, char in enumerate(s):
             if char in usedChar and start <= usedChar[char]:
                 start = usedChar[char] + 1
+                # print(start)
             else:
                 maxLength = max(maxLength,index-start+1)
+                # print(maxLength)
             usedChar[char] = index
+            print(usedChar)
         return maxLength
 
 
