122183b516102b17c1372f45c547234e7eee69ec
Saranyan Senthivel <ssenthivel2@dxc.com>
2020-03-01 20:46:11 -0600

    Create animalCrackers.py

---
+++ personalCoding/animalCrackers.py
@@ -0,0 +1,7 @@
+def animalCrackers(text):
+    wordlist = text.split() #split the text to words
+    # print(wordlist)
+    return wordlist[0][0] == wordlist[1][0] #compare first letter of first word with first letter of 2nd word and return true or false
+
+# the below is the drive code 
+print(animalCrackers('line lain'))
